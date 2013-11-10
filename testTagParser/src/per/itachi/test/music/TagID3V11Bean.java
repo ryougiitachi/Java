92f83de99b140ca02bd7c4edd8618db9408cfe2f
ryougiitachi <ryougiitachi@gmail.com>
2013-11-10 23:02:25 +0800

    Test Github

---
+++ testTagParser/src/per/itachi/test/music/TagID3V11Bean.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package per.itachi.test.music;
+
+/**
+ * @author Ryougi Itachi
+ *
+ */
+public class TagID3V11Bean {
+
+	/**
+	 * 
+	 */
+	public TagID3V11Bean() {
+		super();
+	}
+
+}
