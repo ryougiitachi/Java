fa34c138868fbb2567ae355d957d34308532a493
ryougiitachi <ryougiitachi@gmail.com>
2013-11-11 09:25:52 +0800

    test commit

---
+++ TagParserOnGithub/src/per/itachi/test/bean/TagID3V11Bean.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package per.itachi.test.bean;
+
+/**
+ * @author Administrator
+ *
+ */
+public class TagID3V11Bean {
+
+	/**
+	 * 
+	 */
+	public TagID3V11Bean() {
+		super();
+	}
+
+}
